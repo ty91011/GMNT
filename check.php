a3cd686cf416ca7fb31ec089d3a081b0d17e52c6
Thomas Yu <ty91011@gmail.com>
2018-08-30 00:12:50 -0700

    f

--- check.php
+++ check.php
@@ -14,7 +14,7 @@ select e.tmId, e.datetime, max(created) as lastCached from events e left join ca
 group by e.tmId
 ) a
  where datetime >= NOW() and lastCached <= date_sub(now(), INTERVAL 6 hour) 
-limit 50
+limit 5
 ");
 error_log("BEGIN CRON AUTO CHECK");
 
